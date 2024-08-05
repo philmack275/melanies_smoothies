64ff91e2019dca18b5acdcf074d2b34c5d4b01f6
philmack275 <55976173+philmack275@users.noreply.github.com>
2024-08-05 12:20:46 +1200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,15 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 
+cnx=st.connection("snowflake")
+session = cnx.session()
+
 st.title('Smoothies')
 
 name_on_order=st.text_input('Name')
 st.write('The name on your smoothie will be:',name_on_order)
 
-cnx=st.connection("snowflake")
-session = cnx.session()
 my_dataframe=session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-
 ingredients_list=st.multiselect(
     'Choose up to 5'
     , my_dataframe
