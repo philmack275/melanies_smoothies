02a00adc31e61027853415340994721b9ebc8cae
philmack275 <55976173+philmack275@users.noreply.github.com>
2024-08-05 14:02:36 +1200

    cvc

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,8 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response.json())
+#st.text(fruityvice_response.json())
+fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width=true)
 
 cnx=st.connection("snowflake")
 session = cnx.session()
